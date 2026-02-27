4ea45f93c467c6d0aaf1255da06b25800250b2fe
AYNSJC <chandhokaarya@gmail.com>
2026-02-27 12:55:46 +0530

    Winget search

--- xpac/src/main.rs
+++ xpac/src/main.rs
@@ -1,16 +1,27 @@
 use std::io;
+use std::process::Command;
 
 fn main() {
     let mut running = true;
 
     while running {
         let mut input = String::new();
-
         io::stdin().read_line(&mut input).expect("Failed to read line");
+        let input = input.trim();
 
-        println!("You typed: {}", input.trim());
+        if input.contains("-f") {
+            if let Some((_flag, query)) = input.split_once(' ') {
+                if cfg!(target_os = "windows") {
+                    let output = Command::new("cmd").args(["/C", &format!("winget search {}", query)]).output().expect("failed to execute process");
 
-        if input.trim() == "exit" {
+                    println!("{}", String::from_utf8_lossy(&output.stdout));
+                }
+            } else {
+                println!("Usage: -f <search_term>");
+            }
+        }
+
+        if input == "-q" {
             running = false;
         }
     }
